69454607e9ca9ca78e01633210fe683f04f6d1a6
IAlibay <ialibay@mdanalysis.org>
2022-05-25 18:23:07 +0100

    Add ptp1b

---
+++ openfe_benchmarks/ptp1b.py
@@ -0,0 +1,79 @@
+from rdkit import Chem
+from openfe.setup.lomap_mapper import LomapAtomMapper
+
+from openfe_benchmarks.utils import RBFEBenchmarkSystem
+
+
+def get_system(mappers=[LomapAtomMapper(threed=True),], scorer=None):
+    """
+    Returns a RBFEBenchmarkSystem describing the TYK2 benchmark system
+    with a network of ligand atom mappings defined by the input mappers
+    and scorer.
+
+    Parameters
+    ----------
+    mappers : Iterable[LigandAtomMapper]
+        Mappers to use to create the ligand transformation network.
+    scorer : Union[Callable, None]
+        Scoring function for potential mappings, higher scores indicate
+        worse mappings.
+
+    Returns
+    -------
+    system : RBFEBenchmarkSystem
+        RBFEBenchmarkSystem defining the various components and the
+        ligand network of the TY2K benchmark system
+    """
+
+    connections = [('lig_20667_2qbp', 'lig_23479'),
+                   ('lig_20667_2qbp', 'lig_23482'),
+                   ('lig_20667_2qbp', 'lig_23484'),
+                   ('lig_20667_2qbp', 'lig_23485'),
+                   ('lig_20667_2qbp', 'lig_23486'),
+                   ('lig_20669_2qbr', 'lig_23466'),
+                   ('lig_20669_2qbr', 'lig_23472'),
+                   ('lig_20670_2qbs', 'lig_23330_2qbq'),
+                   ('lig_20670_2qbs', 'lig_23466'),
+                   ('lig_20670_2qbs', 'lig_23477'),
+                   ('lig_20670_2qbs', 'lig_23479'),
+                   ('lig_20670_2qbs', 'lig_23482'),
+                   ('lig_20670_2qbs', 'lig_23483'),
+                   ('lig_23466', 'lig_23475'),
+                   ('lig_23467', 'lig_23466'),
+                   ('lig_23467', 'lig_23468'),
+                   ('lig_23467', 'lig_23469'),
+                   ('lig_23467', 'lig_23470'),
+                   ('lig_23467',  'lig_23473'),
+                   ('lig_23467', 'lig_23474'),
+                   ('lig_23467', 'lig_23475'),
+                   ('lig_23467', 'lig_23476'),
+                   ('lig_23469', 'lig_20669_2qbr'),
+                   ('lig_23469', 'lig_23472'),
+                   ('lig_23471', 'lig_23466'),
+                   ('lig_23471', 'lig_23468'),
+                   ('lig_23471', 'lig_23470'),
+                   ('lig_23473', 'lig_20669_2qbr'),
+                   ('lig_23474', 'lig_23466'),
+                   ('lig_23476', 'lig_23466'),
+                   ('lig_23477', 'lig_23330_2qbq'),
+                   ('lig_23477', 'lig_23466'),
+                   ('lig_23477', 'lig_23467'),
+                   ('lig_23477', 'lig_23479'),
+                   ('lig_23477',  'lig_23482'),
+                   ('lig_23477', 'lig_23483'),
+                   ('lig_23480', 'lig_23479'),
+                   ('lig_23480', 'lig_23482'),
+                   ('lig_23482', 'lig_23479'),
+                   ('lig_23482', 'lig_23485'),
+                   ('lig_23482', 'lig_23486'),
+                   ('lig_23483', 'lig_23479'),
+                   ('lig_23484', 'lig_23479'),
+                   ('lig_23484', 'lig_23482'),
+                   ('lig_23484', 'lig_23485'),
+                   ('lig_23484', 'lig_23486'),
+                   ('lig_23485', 'lig_23479'),
+                   ('lig_23486', 'lig_23479'),
+                   ('lig_23486', 'lig_23485'),]
+
+    return RBFEBenchmarkSystem(system_name="ptp1b", connections=connections,
+                               mappers=mappers, scorer=scorer)
--- openfe_benchmarks/tests/test_systems.py
+++ openfe_benchmarks/tests/test_systems.py
@@ -1,6 +1,6 @@
 import pytest
 
-from openfe_benchmarks import tyk2
+from openfe_benchmarks import tyk2, ptp1b
 
 
 class TestTyk2System:
@@ -19,6 +19,28 @@ class TestTyk2System:
 
     def test_protein_component(self, system):
         assert system.protein_component.name == "tyk2"
+
+    def test_solvent_component(self, system):
+        # TODO use name once we bump up to the next gufe release
+        assert system.solvent_component.smiles == "O"
+
+
+class TestPTP1BSystem:
+    """
+    Tests to check that the ptp1b system is properly created
+    """
+    @pytest.fixture()
+    def system(self):
+        return ptp1b.get_system()
+
+    def test_ligand_components(self, system):
+        assert len(system.ligand_components) == 23
+
+    def test_edges(self, system):
+        assert len(system.ligand_network.edges) == 49
+
+    def test_protein_component(self, system):
+        assert system.protein_component.name == "ptp1b"
 
     def test_solvent_component(self, system):
         # TODO use name once we bump up to the next gufe release
