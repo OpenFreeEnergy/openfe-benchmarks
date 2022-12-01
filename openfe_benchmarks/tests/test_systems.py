58a816ab59e6d5d2465e4a391c763a224676d682
IAlibay <ialibay@mdanalysis.org>
2022-12-01 19:57:40 +0000

    Add new tyk2 system

--- openfe_benchmarks/tests/test_systems.py
+++ openfe_benchmarks/tests/test_systems.py
@@ -49,6 +49,28 @@ class TestOldTyk2System:
         assert system.solvent_component.smiles == "O"
 
 
+class TestNewTyk2System:
+    """
+    Tests to check that the "old" tyk2 system is properly created
+    """
+    @pytest.fixture()
+    def system(self):
+        return tyk2.get_new_system()
+
+    def test_ligand_components(self, system):
+        assert len(system.ligand_components) == 14
+
+    def test_edges(self, system):
+        assert len(system.ligand_network.edges) == 13
+
+    def test_protein_component(self, system):
+        assert system.protein_component.name == "tyk2_new"
+
+    def test_solvent_component(self, system):
+        # TODO use name once we bump up to the next gufe release
+        assert system.solvent_component.smiles == "O"
+
+
 class TestPTP1BSystem:
     """
     Tests to check that the ptp1b system is properly created
