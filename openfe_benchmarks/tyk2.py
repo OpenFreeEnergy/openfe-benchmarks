c28e4b52f0f589b494d04ecc805f417d29b16f4a
IAlibay <ialibay@mdanalysis.org>
2022-12-01 13:47:49 +0000

    Add old tyk2 data

--- openfe_benchmarks/tyk2.py
+++ openfe_benchmarks/tyk2.py
@@ -43,3 +43,56 @@ def get_system(
 
     return RBFEBenchmarkSystem(system_name="tyk2", connections=connections,
                                mappers=mappers, scorer=scorer)
+
+
+def get_old_system(
+    mappers=[LomapAtomMapper(time=20, threed=True, element_change=False, max3d=1),],
+    scorer=None
+):
+    """
+    Returns a RBFEBenchmarkSystem describing the "old" TYK2 benchmark system
+    with a network of ligand atom mappings defined by the input mappers
+    and scorer.
+
+    Parameters
+    ----------
+    mappers : Iterable[LigandAtomMapper]
+        Mappers to use to create the ligand transformation network.
+    scorer : Union[Callable, None]
+        Scoring function for potential mappings, higher scores indicate
+        worse mappings.
+
+    Returns
+    -------
+    system : RBFEBenchmarkSystem
+        RBFEBenchmarkSystem defining the various components and the
+        ligand network of the TYK2 benchmark system
+    """
+
+    connections = [("lig_ejm_31", "lig_ejm_43"),
+                   ("lig_ejm_31", "lig_ejm_45"),
+                   ("lig_ejm_31", "lig_ejm_46"),
+                   ("lig_ejm_31", "lig_ejm_48"),
+                   ("lig_ejm_31", "lig_jmc_28"),
+                   ("lig_ejm_42", "lig_ejm_48"),
+                   ("lig_ejm_42", "lig_ejm_54"),
+                   ("lig_ejm_42", "lig_ejm_55"),
+                   ("lig_ejm_43", "lig_ejm_55"),
+                   ("lig_ejm_44", "lig_ejm_42"),
+                   ("lig_ejm_44", "lig_ejm_55"),
+                   ("lig_ejm_45", "lig_ejm_42"),
+                   ("lig_ejm_47", "lig_ejm_31"),
+                   ("lig_ejm_47", "lig_ejm_55"),
+                   ("lig_ejm_49", "lig_ejm_31"),
+                   ("lig_ejm_49", "lig_ejm_50"),
+                   ("lig_ejm_50", "lig_ejm_42"),
+                   ("lig_ejm_55", "lig_ejm_54"),
+                   ("lig_jmc_23", "lig_ejm_46"),
+                   ("lig_jmc_23", "lig_ejm_55"),
+                   ("lig_jmc_23", "lig_jmc_27"),
+                   ("lig_jmc_23", "lig_jmc_30"),
+                   ("lig_jmc_28", "lig_jmc_27"),
+                   ("lig_jmc_28", "lig_jmc_30"),]
+
+    return RBFEBenchmarkSystem(system_name="tyk2_old", connections=connections,
+                               mappers=mappers, scorer=scorer)
