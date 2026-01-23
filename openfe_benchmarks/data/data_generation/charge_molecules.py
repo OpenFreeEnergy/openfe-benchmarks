c662ae532d3598ebd529c8e09a7ec96b96515bf3
Josh Horton <Josh.Horton@newcastle.ac.uk>
2026-01-23 15:27:16 +0000

    apply PR feedback

--- openfe_benchmarks/data/data_generation/charge_molecules.py
+++ openfe_benchmarks/data/data_generation/charge_molecules.py
@@ -9,16 +9,17 @@ import click
 from rdkit import Chem
 from gufe import SmallMoleculeComponent
 import openfe
-import openff.toolkit
+from openff import toolkit
 from openff.utilities.provenance import get_ambertools_version
 import json
 
 @click.command()
 @click.option("--input-path", type=click.Path(exists=True, dir_okay=False, path_type=pathlib.Path), required=True, help="Path to the input SDF file containing the molecules to be charged.")
 @click.option("--output-dir", type=click.Path(dir_okay=True, file_okay=False, exists=True, path_type=pathlib.Path), required=True, help="Path to the output folder the SDF file with charged molecules will be saved.")
-@click.option("--charge-method", type=click.Choice(["am1bcc", "am1bccelf10"]), default="am1bcc", help="The method to use for charge assignment.")
+@click.option("--charge-method", type=click.Choice(["am1bcc_at", "am1bccelf10", "nagl", "am1bcc_oe"]), default="am1bcc_at", help="The method to use for charge assignment.")
+@click.option("--nagl-model", type=str, default=None, help="Path to the NAGL model to use for charge assignment when using the 'nagl' method if None the latest model will be used.")
 @click.option("--n-cores", type=int, default=1, help="Number of CPU cores to use for parallel processing.")
-def main(input_path: pathlib.Path, output_dir: pathlib.Path, charge_method: str, n_cores: int):
+def main(input_path: pathlib.Path, output_dir: pathlib.Path, charge_method: str, nagl_model: None | str, n_cores: int):
     """Generate partial charges for a set of molecules using OpenFE's bulk charge assignment utility.
 
     Parameters
@@ -28,57 +29,82 @@ def main(input_path: pathlib.Path, output_dir: pathlib.Path, charge_method: str,
     output_dir : pathlib.Path
         Directory where the output SDF file with charged molecules will be saved.
     charge_method : str
-        The method to use for charge assignment. Options are 'am1bcc', 'am1bccelf10', 'gasteiger', and 'am1-mulliken'.
+        The method to use for charge assignment. Options are 'am1bcc_at', 'am1bccelf10', 'nagl', 'am1bcc_oe'.
     n_cores : int
         Number of CPU cores to use for parallel processing.
+    nagl_model
+        Path to the NAGL model to use for charge assignment when using the 'nagl' method if None the latest model will be used.
 
     Notes
     -----
-    - Antechamber will be used for the am1bcc charge assignment method, the charges are calculated at the input geometry.
-    - openeye toolkits is required for am1bccelf10 charge assignment method.
+    - Antechamber will be used for the am1bcc_at charge assignment method, the charges are calculated at the input geometry.
+    - openeye toolkits is required for am1bccelf10 charge assignment method and am1bcc_oe.
     - The output SDF file will include software version metadata as a property for each ligand and will be named <input_name>_<charge_method>.sdf
 
     """
     with Chem.SDMolSupplier(input_path.as_posix(), removeHs=False) as supplier:
         mols = [SmallMoleculeComponent.from_rdkit(mol) for mol in supplier if mol is not None]
-
+        input_order = [mol.name for mol in mols]
         # construct the toolkit backend
         method_to_backend = {
-            "am1bcc": "ambertools",
+            "am1bcc_at": "ambertools",
+            "am1bcc_oe": "openeye",
             "am1bccelf10": "openeye",
+            "nagl": "rdkit"
         }
+        backend = method_to_backend[charge_method]
+
+        # convert the charge method to the expected format for openff
+        openff_charge_method = charge_method.replace("_at", "").replace("_oe", "")
+
         # we need to generate conformers for am1bccelf10 or used the input conformer for other methods which is the None case
         generate_n_conformers = None if charge_method != "am1bccelf10" else 500
+
         charged_ligands = bulk_assign_partial_charges(
             molecules=mols,
             overwrite=True,
-            method=charge_method,
-            toolkit_backend=method_to_backend[charge_method],
+            method=openff_charge_method,
+            toolkit_backend=backend,
             processors=n_cores,
             generate_n_conformers=generate_n_conformers,
-            nagl_model=None
+            nagl_model=nagl_model
         )
+
+        # the multiprocessing can shuffle the order so we need to restore the input order
+        charged_ligands = sorted(charged_ligands, key=lambda x: input_order.index(x.name))
+
         # for each ligand stamp the provenance info as sdf property and write to output sdf
         # generate the provenance info
         provenance = {
             "openfe_version": openfe.__version__,
-            "openff_toolkit_version": openff.toolkit.__version__,
+            "openff_toolkit_version": toolkit.__version__,
             "rdkit_version": Chem.rdBase.rdkitVersion,
             "charge_method": charge_method,
         }
-        if method_to_backend[charge_method] == "ambertools":
+        if backend == "ambertools":
             provenance["ambertools_version"] = get_ambertools_version()
 
-        elif method_to_backend[charge_method] == "openeye":
+        elif backend == "openeye":
             from openeye import oeomega, oequacpac
             provenance["oeomega"] = str(oeomega.OEOmegaGetVersion())
             provenance["oequacpac"] = str(oequacpac.OE_OEQUACPAC_VERSION)
+        elif backend == "rdkit" and charge_method == "nagl":
+            from openff import nagl
+            from openff.nagl_models import get_models_by_type
+            if nagl_model is None:
+                # get the latest production nagl model
+                nagl_model = get_models_by_type(model_type="am1bcc", production_only=True)[-1].name
+            provenance["nagl_version"] = str(nagl.__version__)
+            provenance["nagl_model"] = nagl_model
 
-
+        # construct the output path
         method_to_name = {
-            "am1bcc": "antechamber_am1bcc",
+            "am1bcc_at": "antechamber_am1bcc",
             "am1bccelf10": "openeye_am1bccelf10",
+            "nagl": f"nagl_{nagl_model}",
+            "am1bcc_oe": "openeye_am1bcc"
         }
+
         output_path = output_dir / f"{input_path.stem}_{method_to_name[charge_method]}.sdf"
         with Chem.SDWriter(str(output_path)) as writer:
             for ligand in charged_ligands:
