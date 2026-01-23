5d34187a056fd5f19e9bb72b3d9462420bc952f4
Josh Horton <joshua.horton@openforcefield.org>
2026-01-23 14:41:51 +0000

    Update openfe_benchmarks/data/data_generation/charge_molecules.py
    Co-authored-by: Hannah Baumann <43765638+hannahbaumann@users.noreply.github.com>

--- openfe_benchmarks/data/data_generation/charge_molecules.py
+++ openfe_benchmarks/data/data_generation/charge_molecules.py
@@ -47,7 +47,7 @@ def main(input_path: pathlib.Path, output_dir: pathlib.Path, charge_method: str,
             "am1bcc": "ambertools",
             "am1bccelf10": "openeye",
         }
-        # we need to generate conformers for am1bccelf10 or used the input conformer for other methods which is the None case
+        # we need to generate conformers for am1bccelf10 or use the input conformer for other methods which is the None case
         generate_n_conformers = None if charge_method != "am1bccelf10" else 500
         charged_ligands = bulk_assign_partial_charges(
             molecules=mols,
