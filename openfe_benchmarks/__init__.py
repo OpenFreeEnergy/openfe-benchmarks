e9480a4f1377a353ee79dbf3815997aeadc45230
jaclark5 <jennifer.clark@openforcefield.org>
2026-01-28 11:11:25 -0500

    Add factory method

--- openfe_benchmarks/__init__.py
+++ openfe_benchmarks/__init__.py
@@ -1,7 +1,18 @@
 """OpenFE Benchmarks: Benchmarking systems for OpenFE."""
 
 from importlib.metadata import version
+from loguru import logger
+import sys
 
 __all__ = ()
 
 __version__ = version("openfe_benchmarks")
+
+# Configure logger for the package
+# Remove default handler and add a single configured handler
+logger.remove()
+logger.add(
+    sys.stderr,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <level>{message}</level>",
+    level="INFO"
+)
