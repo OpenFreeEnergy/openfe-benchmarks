21c323446bbafc48e411dc7ca451b8e8957cb9ac
jaclark5 <jennifer.clark@openforcefield.org>
2026-02-05 10:40:36 -0500

    update plan_rbfe

--- openfe_benchmarks/scripts/_example_plan_rbfe.py
+++ openfe_benchmarks/scripts/_example_plan_rbfe.py
@@ -9,7 +9,7 @@ latter files that can be run on an HPC simply with:
 See the [openfe tutorials](https://docs.openfree.energy/en/latest/tutorials/rbfe_cli_tutorial.html)
 for more details.
 """
-import copy
+
 import os
 
 import openfe
@@ -19,17 +19,24 @@ from openfe.protocols.openmm_rfe.equil_rfe_methods import RelativeHybridTopology
 from openfe_benchmarks.data import get_benchmark_data_system
 from openfe_benchmarks.scripts import utils as ofebu
 
-SOLVENT = SolventComponent(positive_ion='Na', negative_ion='Cl', neutralize=True)
+import logging
+
+# Configure logging
+logger = logging.getLogger(__name__)
+
+SOLVENT = SolventComponent(positive_ion="Na", negative_ion="Cl", neutralize=True)
 BENCHMARK_SET = "mcs_docking_set"
 BENCHMARK_SYS = "hne"
-PARTIAL_CHARGE = "nagl_openff-gnn-am1bcc-1.0.0.pt"
-FORCEFIELD = 'openff-2.3.0'
+PARTIAL_CHARGE = "nagl_openff-gnn-am1bcc-1.0.0.pt"  # for the ligand and cofactors
+FORCEFIELD = "openff-2.3.0"  # available in openff-forcefields
 LIG_NETWORK_FILE = "industry_benchmarks_network"
-FILENAME_ALCHEMICALNETWORK = f"alchemical_network_{BENCHMARK_SET}_{BENCHMARK_SYS}_nacl.json"
+FILENAME_ALCHEMICALNETWORK = (
+    f"alchemical_network_{BENCHMARK_SET}_{BENCHMARK_SYS}_nacl.json"
+)
 OUTPUT_DIR = "outputs"
 
-def process_components(benchmark_sys):
 
+def process_components(benchmark_sys):
     """
     Process the components of a benchmark system.
 
@@ -46,19 +53,30 @@ def process_components(benchmark_sys):
 
     if not benchmark_sys.ligand_networks:
         raise ValueError("Valid protein network.json is required.")
-    lig_network = openfe.LigandNetwork.from_json(file=str(benchmark_sys.ligand_networks[LIG_NETWORK_FILE]))
-    ligand_dict = ofebu.process_sdf(benchmark_sys.ligands[PARTIAL_CHARGE], return_dict=True)
+    lig_network = openfe.LigandNetwork.from_json(
+        file=str(benchmark_sys.ligand_networks[LIG_NETWORK_FILE])
+    )
+    ligand_dict = ofebu.process_sdf(
+        benchmark_sys.ligands[PARTIAL_CHARGE], return_dict=True
+    )
     if benchmark_sys.protein is None:
         raise ValueError("Valid protein pdb is required.")
     protein = ProteinComponent.from_pdb_file(str(benchmark_sys.protein))
 
     cofactors = None
     if benchmark_sys.cofactors is not None:
-        cofactors = ofebu.process_sdf(benchmark_sys.ligands[PARTIAL_CHARGE], return_dict=False)
+        cofactors = ofebu.process_sdf(
+            benchmark_sys.ligands[PARTIAL_CHARGE], return_dict=False
+        )
+
+    # apply new partial charges
 
     return lig_network, ligand_dict, protein, cofactors
 
-def compile_network_transformations(ligand_network, solvent, ligands_by_name, protein, cofactors):
+
+def compile_network_transformations(
+    ligand_network, solvent, ligands_by_name, protein, cofactors
+):
     """
     Compile alchemical transformations for a given network.
 
@@ -84,19 +102,15 @@ def compile_network_transformations(ligand_network, solvent, ligands_by_name, pr
     for edge in ligand_network.edges:
         new_edge = openfe.LigandAtomMapping(
             componentA=ligands_by_name[edge.componentA.name],
-            componentB=ligands_by_name[edge.componentB.name], 
+            componentB=ligands_by_name[edge.componentB.name],
             componentA_to_componentB=edge.componentA_to_componentB,
             annotations=edge.annotations,
         )
-    
+
         # create the transformations for the bound and solvent legs
         for leg in ["solvent", "complex"]:
-            system_a_dict = {
-                "ligand": new_edge.componentA, "solvent": solvent
-            }
-            system_b_dict = {
-                "ligand": new_edge.componentB, "solvent": solvent
-            }
+            system_a_dict = {"ligand": new_edge.componentA, "solvent": solvent}
+            system_b_dict = {"ligand": new_edge.componentB, "solvent": solvent}
             if leg == "complex":
                 system_a_dict["protein"] = protein
                 system_b_dict["protein"] = protein
@@ -114,7 +128,7 @@ def compile_network_transformations(ligand_network, solvent, ligands_by_name, pr
 
             # Create protocol with adaptive settings
             # adaptive transformation settings are only supported for RelativeHybridTopologyProtocol currently
-            protocol_settings = copy.deepcopy(RelativeHybridTopologyProtocol.default_settings())
+            protocol_settings = RelativeHybridTopologyProtocol.default_settings()
             protocol_settings.forcefield_settings.small_molecule_forcefield = FORCEFIELD
             transformation_protocol = RelativeHybridTopologyProtocol(
                 settings=RelativeHybridTopologyProtocol._adaptive_settings(
@@ -131,7 +145,7 @@ def compile_network_transformations(ligand_network, solvent, ligands_by_name, pr
                 stateB=system_b,
                 mapping=new_edge,
                 protocol=transformation_protocol,
-                name=name
+                name=name,
             )
             transformations.append(transformation)
 
@@ -148,16 +162,19 @@ def main():
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     benchmark_sys = get_benchmark_data_system(BENCHMARK_SET, BENCHMARK_SYS)
     lig_network, ligand_dict, protein, cofactors = process_components(benchmark_sys)
-    
+
     transformations = compile_network_transformations(
         lig_network, SOLVENT, ligand_dict, protein, cofactors
     )
+
+    # Can be used as input for Alchemiscale
     alchem_network = openfe.AlchemicalNetwork(edges=transformations)
-    alchem_network.to_json(file=os.path.join(OUTPUT_DIR,FILENAME_ALCHEMICALNETWORK))
-    
-    # Write each leg for computation
+    alchem_network.to_json(file=os.path.join(OUTPUT_DIR, FILENAME_ALCHEMICALNETWORK))
+
+    # Can be run HPC3
     for transformation in alchem_network.edges:
         transformation.to_json(os.path.join(OUTPUT_DIR, f"{transformation.name}.json"))
+
 
 if __name__ == "__main__":
     main()
