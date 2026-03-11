448c003aa8d5d7465c211de74748c56c19336b29
jaclark5 <jennifer.clark@openforcefield.org>
2026-03-11 14:54:36 -0400

    Update settings and other review comments

--- openfe_benchmarks/scripts/_example_plan_asfe.py
+++ openfe_benchmarks/scripts/_example_plan_asfe.py
@@ -10,13 +10,13 @@ class for the independent transformations needed in ASFEs.
 import json
 import logging
 from pathlib import Path
+import warnings
 
 from openff.units import unit
 import openfe
 from openfe import SmallMoleculeComponent
 
 from pontibus.components import ExtendedSolventComponent
-from pontibus.utils.molecules import WATER
 from pontibus.protocols.solvation import ASFEProtocol, ASFESettings
 from pontibus.protocols.solvation.settings import PackmolSolvationSettings
 
@@ -44,7 +44,7 @@ BENCHMARK_SYS = "mnsol_neutral"
 PARTIAL_CHARGE = "nagl_openff-gnn-am1bcc-1.0.0.pt"
 FORCEFIELD = "openff-2.3.0"  # available [openmmforcefields SystemGenerator](https://github.com/openmm/openmmforcefields?tab=readme-ov-file#automating-force-field-management-with-systemgenerator)
 OUTPUT_DIR = "outputs"
-FILENAME = f"network_{BENCHMARK_SET}_{BENCHMARK_SYS}_nacl_asfe.json"
+FILENAME = f"network_{BENCHMARK_SET}_{BENCHMARK_SYS}_asfe.json"
 WATER_MODEL = "tip3p"  # or opc
 
 EXPECTED_NETWORKS = []
@@ -83,7 +83,9 @@ def get_chemical_systems(
 
     # Load all molecules (solutes + organic solvents) keyed by molecule name
     mol_dict: dict[str, SmallMoleculeComponent] = ofebu.process_sdf(
-        benchmark_sys.ligands[PARTIAL_CHARGE], return_dict=True
+        benchmark_sys.ligands[PARTIAL_CHARGE],
+        return_dict=True,
+        key_type="inchikey",
     )
     logger.info(
         f"Loaded {len(mol_dict)} molecules from {benchmark_sys.ligands[PARTIAL_CHARGE].name} (charge model: {PARTIAL_CHARGE})"
@@ -91,49 +93,44 @@ def get_chemical_systems(
 
     systems: dict[str, openfe.ChemicalSystem] = {}
     for network_name, entry in exp_data.items():
-        solute_name, solvent_name = entry["solute_name"], entry["solvent_name"]
+        solute_inchikey, solvent_inchikey = (
+            entry["solute_inchikey"],
+            entry["solvent_inchikey"],
+        )
 
-        if solute_name == "water":
-            solute = WATER
-            ### Until Pontibus PR is Merged that defined water component name ###
-            water_dict = solute.to_dict()
-            water_dict["molprops"]["ofe-name"] = "water"
-            solute = SmallMoleculeComponent.from_dict(water_dict)
-            #####################################################################
-        elif solute_name not in mol_dict:
+        if solute_inchikey == "XLYOFNOQVPJJNP-UHFFFAOYNA-N":  # water
+            warnings.warn(
+                f"Cannot obtain SFE of water as a solute. Skipping {network_name}"
+            )
+        elif solute_inchikey not in mol_dict:
             logger.warning(
-                f"Solute '{solute_name}' not found in SDF; skipping network '{network_name}'"
+                f"Solute '{solute_inchikey}' not found in SDF; skipping network '{network_name}'"
             )
             continue
         else:
-            solute = mol_dict[solute_name]
+            solute = mol_dict[solute_inchikey]
 
-        if solvent_name.lower() == "water":
+        if solvent_inchikey == "XLYOFNOQVPJJNP-UHFFFAOYNA-N":  # water
             solvent = ExtendedSolventComponent()
-            ### Until Pontibus PR is Merged that defined water component name ###
-            water_dict = solvent.solvent_molecule.to_dict()
-            water_dict["molprops"]["ofe-name"] = "water"
-            solvent = ExtendedSolventComponent(
-                solvent_molecule=SmallMoleculeComponent.from_dict(water_dict)
-            )
-            #####################################################################
         else:
-            if solvent_name not in mol_dict:
+            if solvent_inchikey not in mol_dict:
                 logger.warning(
-                    f"Solvent '{solvent_name}' not found in SDF; skipping network '{network_name}'"
+                    f"Solvent '{solvent_inchikey}' not found in SDF; skipping network '{network_name}'"
                 )
                 continue
-            solvent = ExtendedSolventComponent(solvent_molecule=mol_dict[solvent_name])
+            solvent = ExtendedSolventComponent(
+                solvent_molecule=mol_dict[solvent_inchikey]
+            )
 
         systems[network_name] = openfe.ChemicalSystem(
             {"solute": solute, "solvent": solvent},
             name=network_name,
         )
         EXPECTED_NETWORKS.append(network_name)
-        if solute_name != "water":
-            EXPECTED_LIGANDS.add(solute_name)
-        if solvent_name != "water":
-            EXPECTED_LIGANDS.add(solvent_name)
+        if solute_inchikey != "XLYOFNOQVPJJNP-UHFFFAOYNA-N":  # water
+            EXPECTED_LIGANDS.add(solute_inchikey)
+        if solvent_inchikey != "XLYOFNOQVPJJNP-UHFFFAOYNA-N":  # water
+            EXPECTED_LIGANDS.add(solvent_inchikey)
 
     logger.info(
         f"Built {len(systems)} ChemicalSystems from experimental data ({len(exp_data) - len(systems)} skipped)"
@@ -160,144 +157,39 @@ def get_water_settings(forcefield: str, water_model: str = "tip3p") -> ASFESetti
     # The settings here are effectively the "fast" settings
     # shown in the validation.
     settings: ASFESettings = ASFEProtocol.default_settings()
+
+    # _______________ Nondefault Settings _________________________________
+    # In Alchemiscale you can set protocol_repeats to 1 and then
+    # run the Transformation task multiple times to get repeats.
+    # Locally, the recommendation would be to set this to 3 so that you can
+    # get a standard deviation uncertainty. It's not super necessary since
+    # SFEs converge well.
     settings.protocol_repeats = 1
+
     settings.solvent_forcefield_settings.forcefields = [
         # To use a custom force field, just pass an OFFXML string
         # just like you would to openff.toolkit.ForceField
         forcefield,
         f"{water_model}.offxml",
     ]
-    settings.vacuum_forcefield_settings.forcefields = [
-        forcefield,  # as above
-    ]
-    settings.vacuum_engine_settings.compute_platform = "CUDA"
-    settings.solvent_engine_settings.compute_platform = "CUDA"
     # For water solvation, use a dodecahedron box with solvent padding.
     # In our tests 1.5 nm padding works for FreeSolv; increase for ligands
     # that can unfold.
     settings.solvation_settings = PackmolSolvationSettings(
-        box_shape="dodecahedron",
+        box_shape="dodecahedron",  # nondefault
         assign_solvent_charges=False,
-        solvent_padding=1.5 * unit.nanometer,
+        solvent_padding=1.5 * unit.nanometer,  # nondefault
     )
-    settings.lambda_settings.lambda_elec = [
-        0.0,
-        0.25,
-        0.5,
-        0.75,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-        1.0,
-    ]
-    settings.lambda_settings.lambda_vdw = [
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.12,
-        0.24,
-        0.36,
-        0.48,
-        0.6,
-        0.7,
-        0.77,
-        0.85,
-        1.0,
-    ]
-    settings.lambda_settings.lambda_restraints = [
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-    ]
-    settings.vacuum_simulation_settings.n_replicas = 14
-    settings.solvent_simulation_settings.n_replicas = 14
-    settings.solvent_simulation_settings.time_per_iteration = 2.5 * unit.picosecond
-    settings.vacuum_simulation_settings.time_per_iteration = 2.5 * unit.picosecond
-    settings.solvent_equil_simulation_settings.equilibration_length_nvt = (
-        0.5 * unit.nanosecond
-    )
-    settings.solvent_equil_simulation_settings.equilibration_length = (
-        0.5 * unit.nanosecond
-    )
-    settings.solvent_equil_simulation_settings.production_length = 9.5 * unit.nanosecond
-    settings.vacuum_equil_simulation_settings.equilibration_length_nvt = None
-    settings.vacuum_equil_simulation_settings.equilibration_length = (
-        0.2 * unit.nanosecond
-    )
-    settings.vacuum_equil_simulation_settings.production_length = 0.5 * unit.nanosecond
-    settings.solvent_simulation_settings.equilibration_length = 1.0 * unit.nanosecond
-    settings.vacuum_simulation_settings.equilibration_length = 0.5 * unit.nanosecond
-    settings.solvent_simulation_settings.production_length = 10.0 * unit.nanosecond
-    settings.vacuum_simulation_settings.production_length = 2.0 * unit.nanosecond
-    return settings
 
-
-def get_nonwater_settings(forcefield: str) -> ASFESettings:
-    """
-    Get settings for solvation free energy calculations (non-water solvents).
-
-    Parameters
-    ----------
-    forcefield : str
-        Name of the force field to use.
-
-    Returns
-    -------
-    ASFESettings
-        Settings object for ASFEProtocol.
-    """
-    # The settings here are effectively the "fast" settings
-    # shown in the validation.
-    settings: ASFESettings = ASFEProtocol.default_settings()
-    # Because it's Alchemiscale, you set protocol_repeats to 1 and then
-    # run the Transformation task multiple times to get repeats.
-    # Locally, the recommendation would be to set this to 3 so that you can
-    # get a standard deviation uncertainty. It's not super necessary since
-    # SFEs converge well, but hey with Alchemiscale why not?!
-    settings.protocol_repeats = 1
-    settings.solvent_forcefield_settings.forcefields = [
-        # To use a custom force field, just pass an OFFXML string
-        # just like you would to openff.toolkit.ForceField
-        forcefield,
-    ]
     settings.vacuum_forcefield_settings.forcefields = [
         forcefield,  # as above
     ]
-    settings.vacuum_engine_settings.compute_platform = "CUDA"
-    settings.solvent_engine_settings.compute_platform = "CUDA"
-    settings.solvation_settings = PackmolSolvationSettings(
-        # In our tests 750 gave quasi equivalent results to the 1999 used in the Sage
-        # benchmarks
-        number_of_solvent_molecules=750,
-        box_shape="cube",
-        # We set assign_solvent_charges to True because we don't have LibraryCharges.
-        # If False it will only attempt to use LibraryCharges.
-        # Note that if True and you don't have any charges on the SmallMoleculeComponent
-        # passed to ExtendedSolventComponent, the Protocol will attempt to automatically
-        # assign partial charges (default is AmberTools am1bcc, but it's controllable
-        # using `partial_charge_settings`.
-        assign_solvent_charges=True,
-        solvent_padding=None,
-    )
+
+    # _______________ Default Settings _________________________________
+    # This is the pre-alchemical equilibration lengths
+    # NVT equilibration -> NPT equilibration -> NPT "production" (more equilibration)
+    # In vacuum, we set the NVT equilibration to None since it's all gas phase
+
     # Below are the default lambda & replica settings, so you don't have to
     # actually set them, but if you want to change things, you can alter them
     # by defining them this way. Note: you have to update n_replica to match
@@ -350,17 +242,144 @@ def get_nonwater_settings(forcefield: str) -> ASFESettings:
         0.0,
         0.0,
     ]
-    settings.vacuum_simulation_settings.n_replicas = 14
     settings.solvent_simulation_settings.n_replicas = 14
-    # This set the time per replica exchange, the default is 1 ps but
-    # hurts performance. I would recommend 2.5 ps
+    settings.vacuum_simulation_settings.n_replicas = 14
+
+    ####### Solvent Settings #############
+    settings.solvent_engine_settings.compute_platform = "CUDA"
     settings.solvent_simulation_settings.time_per_iteration = 2.5 * unit.picosecond
+    settings.solvent_equil_simulation_settings.equilibration_length_nvt = (
+        0.5 * unit.nanosecond
+    )
+    settings.solvent_equil_simulation_settings.production_length = 9.5 * unit.nanosecond
+    settings.solvent_simulation_settings.equilibration_length = 1.0 * unit.nanosecond
+    settings.solvent_simulation_settings.production_length = 10.0 * unit.nanosecond
+
+    ####### Vacuum Settings #############
+    settings.vacuum_engine_settings.compute_platform = "CUDA"
     settings.vacuum_simulation_settings.time_per_iteration = 2.5 * unit.picosecond
-    # Below are the default simulation lengths we use in Pontibus,
-    # so you don't need to set them. However, you can do so manually
+    settings.solvent_equil_simulation_settings.equilibration_length = (
+        0.5 * unit.nanosecond
+    )
+    settings.vacuum_equil_simulation_settings.equilibration_length_nvt = None
+    settings.vacuum_equil_simulation_settings.equilibration_length = (
+        0.2 * unit.nanosecond
+    )
+    settings.vacuum_equil_simulation_settings.production_length = 0.5 * unit.nanosecond
+    settings.vacuum_simulation_settings.equilibration_length = 0.5 * unit.nanosecond
+    settings.vacuum_simulation_settings.production_length = 2.0 * unit.nanosecond
+    return settings
+
+
+def get_nonwater_settings(forcefield: str) -> ASFESettings:
+    """
+    Get settings for solvation free energy calculations (non-water solvents).
+
+    Parameters
+    ----------
+    forcefield : str
+        Name of the force field to use.
+    assign_solvent_charges : bool, default=False
+        Whether to assign solvent charges or not. We expect to set them in all cases but water.
+
+    Returns
+    -------
+    ASFESettings
+        Settings object for ASFEProtocol.
+    """
+    # The settings here are effectively the "fast" settings
+    # shown in the validation.
+    settings: ASFESettings = ASFEProtocol.default_settings()
+
+    # _______________ Nondefault Settings _________________________________
+    # In Alchemiscale you can set protocol_repeats to 1 and then
+    # run the Transformation task multiple times to get repeats.
+    # Locally, the recommendation would be to set this to 3 so that you can
+    # get a standard deviation uncertainty. It's not super necessary since
+    # SFEs converge well.
+    settings.protocol_repeats = 1
+
+    ######## Solvent Settings ###########
+    settings.solvent_forcefield_settings.forcefields = [
+        # To use a custom force field, just pass an OFFXML string
+        # just like you would to openff.toolkit.ForceField
+        forcefield,
+    ]
+    settings.solvation_settings = PackmolSolvationSettings(
+        target_density=0.95 * unit.grams / unit.mL,  # current default
+        box_shape="cube",
+        assign_solvent_charges=False,
+        solvent_padding=None,  # nondefault
+    )
+
+    ######## Vacuum Settings ###########
+    settings.vacuum_forcefield_settings.forcefields = [
+        forcefield,  # as above
+    ]
+
+    # _______________ Default Settings _________________________________
     # This is the pre-alchemical equilibration lengths
     # NVT equilibration -> NPT equilibration -> NPT "production" (more equilibration)
     # In vacuum, we set the NVT equilibration to None since it's all gas phase
+
+    # Below are the default lambda & replica settings, so you don't have to
+    # actually set them, but if you want to change things, you can alter them
+    # by defining them this way. Note: you have to update n_replica to match
+    # the number of lambda windows (and all lambda window lists must be of the same length).
+    settings.lambda_settings.lambda_elec = [
+        0.0,
+        0.25,
+        0.5,
+        0.75,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+    ]
+    settings.lambda_settings.lambda_vdw = [
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.12,
+        0.24,
+        0.36,
+        0.48,
+        0.6,
+        0.7,
+        0.77,
+        0.85,
+        1.0,
+    ]
+    settings.lambda_settings.lambda_restraints = [
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+    settings.solvent_simulation_settings.n_replicas = 14
+    settings.vacuum_simulation_settings.n_replicas = 14
+
+    ######## Solvent Settings ###########
+    settings.solvent_engine_settings.compute_platform = "CUDA"
+    settings.solvent_simulation_settings.time_per_iteration = 2.5 * unit.picosecond
     settings.solvent_equil_simulation_settings.equilibration_length_nvt = (
         0.5 * unit.nanosecond
     )
@@ -368,17 +387,22 @@ def get_nonwater_settings(forcefield: str) -> ASFESettings:
         0.5 * unit.nanosecond
     )
     settings.solvent_equil_simulation_settings.production_length = 9.5 * unit.nanosecond
+
+    settings.solvent_simulation_settings.equilibration_length = 1.0 * unit.nanosecond
+    settings.solvent_simulation_settings.production_length = 10.0 * unit.nanosecond
+
+    ######## Vacuum Settings ###########
+    settings.vacuum_engine_settings.compute_platform = "CUDA"
+    settings.vacuum_simulation_settings.time_per_iteration = 2.5 * unit.picosecond
     settings.vacuum_equil_simulation_settings.equilibration_length_nvt = None
     settings.vacuum_equil_simulation_settings.equilibration_length = (
         0.2 * unit.nanosecond
     )
     settings.vacuum_equil_simulation_settings.production_length = 0.5 * unit.nanosecond
-    # This is the alchemical equilibration length
-    settings.solvent_simulation_settings.equilibration_length = 1.0 * unit.nanosecond
+
     settings.vacuum_simulation_settings.equilibration_length = 0.5 * unit.nanosecond
-    # This is the alchemical production length
-    settings.solvent_simulation_settings.production_length = 10.0 * unit.nanosecond
     settings.vacuum_simulation_settings.production_length = 2.0 * unit.nanosecond
+
     return settings
 
 
@@ -407,7 +431,7 @@ def compile_transformations(
         "nonaqueous": get_nonwater_settings(FORCEFIELD),
     }
     for sys_name, system_solvated_solute in sorted(systems_by_name.items()):
-        # An SFE transformation in GUFE formalism is defined as
+        # An SFE transformation in pontibus formalism is defined as
         # going from a solute + solvent (stateA) to just solvent (stateB)
         # The vacuum states are created automatically, and this transformation
         # includes both the vacuum and solvent legs
@@ -560,9 +584,6 @@ def validate_asfe_network(network_file: Path) -> list[str]:
                 f"Ligand '{ligand.name}' in '{chem_system.key}' is missing partial charges"
             )
 
-    print(
-        len(EXPECTED_LIGANDS), len(found_ligands), len(EXPECTED_LIGANDS - found_ligands)
-    )
     missing_ligs = EXPECTED_LIGANDS - found_ligands
     if missing_ligs:
         errors.append(f"Ligands not found in network: {sorted(missing_ligs)}")
